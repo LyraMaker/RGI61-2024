28dc6ea440bbb7e41b20e825837bfba9268410fc
LyraMaker <gabriel.lyrat@gmail.com>
2024-09-05 13:31:30 -0300

    Listando cadastro

--- Programação Web/pratica-banco/public/index.php
+++ Programação Web/pratica-banco/public/index.php
@@ -4,8 +4,12 @@ require __DIR__ . "/../app/Data/ConectarBanco.php";
 
 //Ele escreverá o endereço completo a partir do local que você está.
 
-$sql = "CREATE TABLE IF NOT EXISTS ALUNO(
-matricula VARCHAR(5) NOT NULL PRIMARY KEY,
-nome TEXT)";
+$sql = "SELECT * FROM ALUNO";
 
-$pdo->query($sql);
+$valores = $pdo->query($sql);
+
+$alunos = $valores->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($alunos as $aluno) {
+    echo $aluno["matricula"]." ".$aluno["nome"]."<br>";
+}
